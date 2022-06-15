c13a517c990c217f0b3d5d15ea0abdaa64d00502
TheMrM <56775033+TheMrM@users.noreply.github.com>
2022-06-15 09:18:51 +0300

    Add files via upload

---
+++ SoundBoard/script.js
@@ -0,0 +1,25 @@
+const sounds = ['DankMeme', 'Duke', 'FallDown', 'Microwave', 'Suspenseful']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
